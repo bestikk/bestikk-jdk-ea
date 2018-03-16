c45edcbc5947a58dcd1c8f238cd929601a987c62
Guillaume Grossetie <ggrossetie@gmail.com>
2018-03-16 11:41:34 +0100

    Use the new website http://jdk.java.net
    - Uses Promise instead of callback
    - Requires Node 6+
    - Uses ECMAScript 6 syntax (const, let, arrow...)

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-var child_process = require('child_process');
-var fs = require('fs');
-var path = require('path');
-var https = require('https');
-var http = require('http');
-var os = require('os');
-var zlib = require('zlib');
-var tar = require('tar-fs');
-var async = require('async');
-var log = require('bestikk-log');
+const child_process = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const http = require('http');
+const os = require('os');
+const zlib = require('zlib');
+const tar = require('tar-fs');
+const log = require('bestikk-log');
 
-
-var JDKEA = function() {
-  this.jdk8EAName = 'jdk1.8.0-ea';
-  this.jdk9EAName = 'jdk1.9.0-ea';
-}
-
-var deleteFolderRecursive = function(path) {
-  var files = [];
+const deleteFolderRecursive = (path) => {
+  let files = [];
   if (fs.existsSync(path)) {
     files = fs.readdirSync(path);
-    files.forEach(function(file){
-      var curPath = path + "/" + file;
+    files.forEach(function (file) {
+      const curPath = path + "/" + file;
       if (fs.lstatSync(curPath).isDirectory()) { // recurse
         deleteFolderRecursive(curPath);
       } else { // delete file
@@ -32,7 +25,7 @@ var deleteFolderRecursive = function(path) {
 };
 
 // https://github.com/jprichardson/node-fs-extra/blob/master/lib/mkdirs/mkdirs-sync.js
-var mkdirsSync = function(p, made) {
+const mkdirsSync = (p, made) => {
   p = path.resolve(p);
   try {
     fs.mkdirSync(p);
@@ -48,7 +41,7 @@ var mkdirsSync = function(p, made) {
       // there already.  If so, then hooray!  If not, then something
       // is borked.
       default:
-        var stat;
+        let stat;
         try {
           stat = fs.statSync(p);
         } catch (err1) {
@@ -59,9 +52,9 @@ var mkdirsSync = function(p, made) {
     }
   }
   return made;
-}
+};
 
-var execSync = function(command) {
+const execSync = (command) => {
   log.debug(command);
   if (!process.env.DRY_RUN) {
     stdout = child_process.execSync(command);
@@ -69,37 +62,32 @@ var execSync = function(command) {
   }
 };
 
-var mkdirSync = function(path) {
-  if (!fs.existsSync(path)) {
-    fs.mkdirSync(path);
-  }
-};
-
-var getContentFromURL = function(source, target, callback) {
+const getContentFromURL = (source, target) => new Promise((resolve) => {
   log.transform('get', source, target);
-  var targetStream = fs.createWriteStream(target);
-  var downloadModule;
+  const targetStream = fs.createWriteStream(target);
+  let downloadModule;
   // startWith alternative
   if (source.lastIndexOf('https', 0) === 0) {
     downloadModule = https;
   } else {
     downloadModule = http;
   }
-  downloadModule.get(source, function(response) {
+  downloadModule.get(source, function (response) {
     response.pipe(targetStream);
     targetStream.on('finish', function () {
-      targetStream.close(callback);
+      targetStream.close();
+      resolve();
     });
   });
-};
+});
 
-var untar = function(source, baseDirName, destinationDir, callback) {
-  log.transform('untar', source, destinationDir + '/' + baseDirName); 
-  var stream = fs.createReadStream(source).pipe(zlib.createGunzip()).pipe(tar.extract(destinationDir, {
+const untar = (source, baseDirName, destinationDir) => new Promise((resolve) => {
+  log.transform('untar', source, destinationDir + '/' + baseDirName);
+  const stream = fs.createReadStream(source).pipe(zlib.createGunzip()).pipe(tar.extract(destinationDir, {
     map: function (header) {
       // REMIND Do NOT user path.sep!
       // In this case, even on Windows, the separator is '/'.
-      var paths = header.name.split('/');
+      const paths = header.name.split('/');
       // replace base directory with 'baseDirName'
       paths.shift();
       paths.unshift(baseDirName);
@@ -108,95 +96,109 @@ var untar = function(source, baseDirName, destinationDir, callback) {
     }
   }));
   stream.on('finish', function () {
-    callback();
+    resolve();
   });
-};
+});
 
-var isWin = function() {
-  return /^win/.test(process.platform);
-};
+const isWin = () => /^win/.test(process.platform);
 
-var jdkDownloadURL = function(jdkId, url, callback) {
-  https.get(url, function(result) {
-    var data = [];
+const jdk8DownloadURL = () => new Promise((resolve, reject) => {
+  const jdkId = isWin() ? 'winOffline64JDK' : 'lin64JDKrpm';
+  http.get('http://jdk.java.net/8/', (result) => {
+    const data = [];
     result.setEncoding('utf8');
-    result.on('data', function(chunk) {
+    result.on('data', (chunk) => {
       data.push(chunk);
     });
-    result.on('end', function(){
-      var html = data.join('');
-      var jdkURLRegexp = new RegExp('document\\.getElementById\\(\\"' + jdkId + '\\"\\)\\.href = \\"http:\\/\\/www.java.net\\/download\\/(.*)\\";');
-      var match = jdkURLRegexp.exec(html)[1];
-      // Avoid redirection http -> https
-      var jdkURL = 'http://download.java.net/' + match;
-      callback(jdkURL);
+    result.on('end', () => {
+      const html = data.join('');
+      const jdkURLRegexp = new RegExp('document\\.getElementById\\(\\"' + jdkId + '\\"\\)\\.href = "(.*)";');
+      const jdkURL = jdkURLRegexp.exec(html)[1];
+      resolve(jdkURL);
     });
-  }).on('error', function(e) {
+  }).on('error', (e) => {
     console.error(e);
+    reject(e);
   });
+});
+
+const jdk9DownloadURL = () => new Promise((resolve, reject) => {
+  http.get('http://jdk.java.net/9/', (result) => {
+    const data = [];
+    result.setEncoding('utf8');
+    result.on('data', function (chunk) {
+      data.push(chunk);
+    });
+    result.on('end', function () {
+      const html = data.join('');
+      let jdkURLRegexp;
+      if (isWin()) {
+        jdkURLRegexp = new RegExp('href="(https:\/\/download.java.net\/java.*_windows-x64.*)"');
+      } else {
+        jdkURLRegexp = new RegExp('href="(https:\/\/download.java.net\/java.*_linux-x64.*)"');
+      }
+      const jdkURL = jdkURLRegexp.exec(html)[1];
+      resolve(jdkURL);
+    });
+  }).on('error', function (e) {
+    console.error(e);
+    reject(e);
+  });
+});
+
+const download = (url, destination) => {
+  log.task('download ' + url);
+  if (fs.existsSync(destination)) {
+    log.info('File ' + destination + ' already exists, skipping download');
+    return Promise.resolve();
+  }
+  log.info('Starting download...');
+  return getContentFromURL(url, destination);
 };
 
-var jdk8DownloadURL = function(callback) {
-  var jdkId = isWin() ? 'winOffline64JDK' : 'lin64JDKrpm';
-  jdkDownloadURL(jdkId, 'https://jdk8.java.net/download.html', callback);
-};
+const install = (installDir, jdkName, jdkURL) => {
+  const suffix = jdkURL.endsWith('.exe') ? '.exe' : '.tar.gz';
+  const jdkEADownloadDestination = os.tmpdir() + path.sep + jdkName + suffix;
 
-var jdk9DownloadURL = function(callback) {
-  var jdkId = isWin() ? 'winOffline64JDK' : 'lin64JDKrpm';
-  jdkDownloadURL(jdkId, 'https://jdk9.java.net/download/', callback);
-};
-
-var install = function(installDir, jdkName, jdkDownloadURLFunction, callback) {
-  var jdkEADownloadDestination = isWin() ? os.tmpdir() + path.sep + jdkName + '.exe' : os.tmpdir() + path.sep + jdkName + '.tar.gz';
-
-  function waitWindowsInstallCompletion(jdkInstallDir) {
+  const waitWindowsInstallCompletion = (jdkInstallDir) => {
     if (!fs.existsSync(jdkInstallDir + path.sep + 'bin' + path.sep + 'jjs.exe')
       || !fs.existsSync(jdkInstallDir + path.sep + 'bin' + path.sep + 'javac.exe')
       || !fs.existsSync(jdkInstallDir + path.sep + 'bin' + path.sep + 'java.exe')) {
       setTimeout(waitWindowsInstallCompletion(jdkInstallDir), 1000);
     }
-  }
+  };
 
-  async.series([
-    function(callback) {
-      deleteFolderRecursive(installDir);
-      mkdirsSync(installDir);
-      callback();
-    },
-    function(callback) {
-      log.task('download ' + jdkName);
-      if (fs.existsSync(jdkEADownloadDestination)) {
-        log.info('File ' + jdkEADownloadDestination + ' already exists, skipping download');
-        callback();
-      } else {
-        log.info('Starting download...');
-        jdkDownloadURLFunction(function(jdkURL) {
-          getContentFromURL(jdkURL, jdkEADownloadDestination, callback);
-        });
-      }
-    },
-    function(callback) {
-      if (isWin()) {
-        winInstallDir = installDir.replace(/\\\//, '\\\\').replace(/\//, '\\\\');
+  deleteFolderRecursive(installDir);
+  mkdirsSync(installDir);
+
+  return download(jdkURL, jdkEADownloadDestination)
+    .then(() => {
+      if (jdkURL.endsWith(".exe")) {
+        const winInstallDir = installDir.replace(/\\\//, '\\\\').replace(/\//, '\\\\');
         execSync(jdkEADownloadDestination + ' /s INSTALLDIR="%CD%\\' + winInstallDir + '"');
         waitWindowsInstallCompletion(installDir);
-        callback();
-      } else {
-        log.task('uncompress ' + jdkName);
-        untar(jdkEADownloadDestination, '', installDir, callback);
+        return Promise.resolve();
       }
-    }
-  ], function() {
-    typeof callback === 'function' && callback();
-  });
+      log.task('uncompress ' + jdkName);
+      return untar(jdkEADownloadDestination, '', installDir);
+    });
 };
 
-JDKEA.prototype.installJDK8EA = function(installDir, callback) {
-  install(installDir, this.jdk8EAName, jdk8DownloadURL, callback);
-};
+class JDKEA {
+  constructor () {
+    this.jdk8EAName = 'jdk1.8.0-ea';
+    this.jdk9EAName = 'jdk1.9.0-ea';
+  };
 
-JDKEA.prototype.installJDK9EA = function(installDir, callback) {
-  install(installDir, this.jdk9EAName, jdk9DownloadURL, callback);
-};
+  installJDK8EA (installDir) {
+    return jdk8DownloadURL()
+      .then((jdkDownloadURL) => install(installDir, this.jdk8EAName, jdkDownloadURL));
+  };
+
+  installJDK9EA (installDir) {
+    return jdk9DownloadURL()
+      .then((jdkDownloadURL) => install(installDir, this.jdk9EAName, jdkDownloadURL));
+  };
+}
 
 module.exports = new JDKEA();
--- spec/spec.js
+++ spec/spec.js
@@ -1,41 +1,35 @@
-var child_process = require('child_process');
-var fs = require('fs');
-var chai = require('chai');
-var assert = chai.assert;
+const child_process = require('child_process');
+const fs = require('fs');
+const chai = require('chai');
+const assert = chai.assert;
 
-var main = require('../index.js');
+const jdkEA = require('../index.js');
 
-describe('Bestikk', function() {
-  describe('#installJDK9EA', function() {
-    it('should install JDK 9 EA in test_install directory', function(done) {
+describe('Bestikk', function () {
+  describe('#installJDK9EA', function () {
+    it('should install JDK 9 EA in test_install directory', function (done) {
       this.timeout(300000); // 5 minutes
-      main.installJDK9EA('test_install/jdk9', function(err) {
-        if (err) {
-          done(err);
-        } else {
+      jdkEA.installJDK9EA('test_install/jdk9')
+        .then(() => {
           assert.equal(fs.existsSync('test_install/jdk9/bin/java'), true);
-          var result = child_process.spawnSync('test_install/jdk9/bin/java', ['-version'], {encoding: 'utf8'});
-          assert.include(result.stderr, '9-ea');
+          const result = child_process.spawnSync('test_install/jdk9/bin/java', ['-version'], {encoding: 'utf8'});
+          assert.include(result.stderr, 'openjdk version "9');
           done();
-        }
-      });
+        });
     });
   });
 
-  describe('#installJDK8EA', function() {
-    it('should install JDK 8 EA in test_install directory', function(done) {
+  describe('#installJDK8EA', function () {
+    it('should install JDK 8 EA in test_install directory', function (done) {
       this.timeout(300000); // 5 minutes
-      main.installJDK8EA('test_install/jdk8', function(err) {
-        if (err) {
-          done(err);
-        } else {
+      jdkEA.installJDK8EA('test_install/jdk8')
+        .then(() => {
           assert.equal(fs.existsSync('test_install/jdk8/bin/java'), true);
-          var result = child_process.spawnSync('test_install/jdk8/bin/java', ['-version'], {encoding: 'utf8'});
+          const result = child_process.spawnSync('test_install/jdk8/bin/java', ['-version'], {encoding: 'utf8'});
           assert.include(result.stderr, '1.8.0');
           assert.include(result.stderr, 'ea');
           done();
-        }
-      });
+        });
     });
   });
 });
